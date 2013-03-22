d4ac7a9561de5d43cc1f969e2afe39276186abc0
Dark Arc <theexcel@rocketmail.com>
2013-03-22 14:42:55 -0400

    Improved Anti-Lighthack system

--- src/main/java/com/skelril/aurora/anticheat/AntiLightHackComponent.java
+++ src/main/java/com/skelril/aurora/anticheat/AntiLightHackComponent.java
@@ -10,15 +10,18 @@ import com.zachsthings.libcomponents.ComponentInformation;
 import com.zachsthings.libcomponents.Depend;
 import com.zachsthings.libcomponents.InjectComponent;
 import com.zachsthings.libcomponents.bukkit.BukkitComponent;
-import org.bukkit.EntityEffect;
 import org.bukkit.Server;
+import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -47,24 +50,53 @@ public class AntiLightHackComponent extends BukkitComponent implements Listener 
 
         Player player = event.getPlayer();
         Block block = event.getBlock();
+        Block pBlock = player.getLocation().getBlock();
+        Block pEyeBlock = pBlock.getRelative(BlockFace.UP);
 
-        if (!adminComponent.isAdmin(player)
-                && !EnvironmentUtil.isWater(block)
-                && player.getLocation().getBlock().getLightLevel() == 0
-                && player.getEyeLocation().getBlock().getLightLevel() == 0
-                && !player.hasPotionEffect(PotionEffectType.NIGHT_VISION)
-                && ChanceUtil.getChance(3)
-                && (EnvironmentUtil.isValuableOre(block) || block.getTypeId() == BlockID.STONE)) {
-            int playerHealth = player.getHealth();
-            if (playerHealth - 1 >= 0) {
-                player.setHealth(playerHealth - 1);
-                player.playEffect(EntityEffect.HURT);
-                ChatUtil.sendWarning(player, "You hurt yourself attempting to mine in the dark.");
-            } else {
-                player.setHealth(0);
-                player.playEffect(EntityEffect.DEATH);
-                ChatUtil.sendWarning(player, "You kill yourself attempting to mine in the dark.");
-            }
+        if (!adminComponent.isAdmin(player) && isProtectedBlock(block.getTypeId())) {
+
+            // Light
+            int light = pBlock.getLightLevel();
+            light += pEyeBlock.getLightLevel();
+            if (light > 0) return;
+
+            // Environment
+            if (!isValidEnvironment(pBlock)) return;
+
+            // Chanced
+            if (!ChanceUtil.getChance(3)) return;
+
+            // Potions
+            if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) return;
+
+            // Damage
+            ChatUtil.sendWarning(player, textOps[ChanceUtil.getRandom(textOps.length) - 1]);
+            player.damage(ChanceUtil.getRandom(5));
         }
+    }
+
+    private static String[] textOps = new String[4];
+
+    static {
+        textOps[0] = "You swing your pickaxe at your foot sometimes yelling... ouch!";
+        textOps[1] = "Your pickaxe suddenly came in contact with your eye.";
+        textOps[2] = "You stumble and trip over a rock.";
+        textOps[3] = "You suddenly find yourself falling from the... Oh wait, you just bashed your head off a rock.";
+    }
+
+    private static List<Biome> biomeBlackList = new ArrayList<>();
+
+    static {
+        biomeBlackList.add(Biome.HELL);
+    }
+
+    private boolean isValidEnvironment(Block block) {
+
+        return !biomeBlackList.contains(block.getBiome()) && !EnvironmentUtil.isWater(block.getTypeId());
+    }
+
+    private boolean isProtectedBlock(int id) {
+
+        return EnvironmentUtil.isValuableOre(id) || id == BlockID.STONE;
     }
 }
