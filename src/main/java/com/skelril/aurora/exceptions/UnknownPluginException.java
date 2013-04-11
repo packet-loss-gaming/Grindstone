fa4827e50bc379d32c4c693b6edcf092a636f879
Dark Arc <theexcel@rocketmail.com>
2013-04-11 17:35:05 -0400

    Renamed UnkownPluginException to UnknownPluginException

---
+++ src/main/java/com/skelril/aurora/exceptions/UnknownPluginException.java
@@ -0,0 +1,14 @@
+package com.skelril.aurora.exceptions;
+
+public class UnknownPluginException extends Exception {
+
+    public UnknownPluginException() {
+
+        super();
+    }
+
+    public UnknownPluginException(String message) {
+
+        super(message);
+    }
+}
