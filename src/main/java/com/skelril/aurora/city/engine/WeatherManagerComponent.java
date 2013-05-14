6aa42e915830a9bd7b2e2b2c88ae5fdb0e0dcc14
Dark Arc <theexcel@rocketmail.com>
2013-05-13 20:04:55 -0400

    Notify players with weather hidden about thunder storms

--- src/main/java/com/skelril/aurora/city/engine/WeatherManagerComponent.java
+++ src/main/java/com/skelril/aurora/city/engine/WeatherManagerComponent.java
@@ -11,23 +11,35 @@ import org.bukkit.Server;
 import org.bukkit.WeatherType;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.weather.ThunderChangeEvent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Author: Turtle9598
  */
 @ComponentInformation(friendlyName = "Weather Manager", desc = "Turn off the storm!")
-public class WeatherManagerComponent extends BukkitComponent {
+public class WeatherManagerComponent extends BukkitComponent implements Listener {
 
     private final CommandBook inst = CommandBook.inst();
     private final Logger log = inst.getLogger();
     private final Server server = CommandBook.server();
 
+    private List<Player> enabledFor = new ArrayList<>();
+
     @Override
     public void enable() {
 
         registerCommands(Commands.class);
+
+        //noinspection AccessStaticViaInstance
+        inst.registerEvents(this);
     }
 
     public class Commands {
@@ -40,13 +52,33 @@ public class WeatherManagerComponent extends BukkitComponent {
             if (!(sender instanceof Player)) throw new CommandException("You must be a player to use this command.");
 
             if (args.hasFlag('r')) {
+                enabledFor.remove(sender);
                 ((Player) sender).resetPlayerWeather();
                 ChatUtil.sendNotice(sender, "Storms are no longer hidden.");
             } else {
+                enabledFor.add((Player) sender);
                 ((Player) sender).setPlayerWeather(WeatherType.CLEAR);
                 ChatUtil.sendNotice(sender, "Storms are now hidden.");
                 ChatUtil.sendNotice(sender, "To show storms again please use /stopweather -r.");
             }
         }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onThunderChange(ThunderChangeEvent event) {
+
+        String state = event.toThunderState() ? "starting" : "ending";
+        for (Player player : Collections.synchronizedList(enabledFor)) {
+            if (player.getWorld().equals(event.getWorld())) {
+                ChatUtil.sendNotice(player, "A thunder storm is " + state + " on your world.");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+
+        Player player = event.getPlayer();
+        if (enabledFor.contains(player)) enabledFor.remove(player);
     }
 }
