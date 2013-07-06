559a0ed0b88f4b8cd10aede08fce403d82d8a413
Dark Arc <theexcel@rocketmail.com>
2013-07-06 19:20:58 -0400

    Implemented the anti mycelium spread

--- src/main/java/com/skelril/aurora/city/engine/BiomeManagementComponent.java
+++ src/main/java/com/skelril/aurora/city/engine/BiomeManagementComponent.java
@@ -9,26 +9,20 @@ import com.skelril.aurora.jail.JailComponent;
 import com.skelril.aurora.util.ChanceUtil;
 import com.skelril.aurora.util.ChatUtil;
 import com.skelril.aurora.util.EnvironmentUtil;
-import com.skelril.aurora.util.LocationUtil;
 import com.zachsthings.libcomponents.ComponentInformation;
 import com.zachsthings.libcomponents.Depend;
 import com.zachsthings.libcomponents.InjectComponent;
 import com.zachsthings.libcomponents.bukkit.BukkitComponent;
 import com.zachsthings.libcomponents.config.ConfigurationBase;
 import com.zachsthings.libcomponents.config.Setting;
-import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Server;
-import org.bukkit.World;
 import org.bukkit.block.Biome;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -37,9 +31,9 @@ import java.util.logging.Logger;
 /**
  * Author: Turtle9598
  */
-@ComponentInformation(friendlyName = "Biome Effects", desc = "Biome Effects.")
+@ComponentInformation(friendlyName = "Biome Management", desc = "Biome Management component.")
 @Depend(plugins = {"Vault"}, components = {JailComponent.class, AdminComponent.class})
-public class BiomeEffectsComponent extends BukkitComponent implements Listener, Runnable {
+public class BiomeManagementComponent extends BukkitComponent implements Listener, Runnable {
 
     private final CommandBook inst = CommandBook.inst();
     private final Logger log = inst.getLogger();
@@ -50,7 +44,6 @@ public class BiomeEffectsComponent extends BukkitComponent implements Listener, 
     @InjectComponent
     private AdminComponent adminComponent;
 
-    private static Permission permission = null;
     private LocalConfiguration config;
 
     @Override
@@ -58,7 +51,7 @@ public class BiomeEffectsComponent extends BukkitComponent implements Listener, 
 
         //noinspection AccessStaticViaInstance
         inst.registerEvents(this);
-        setupPermissions();
+
         config = configure(new LocalConfiguration());
         server.getScheduler().scheduleSyncRepeatingTask(inst, this, 20 * 2, 20 * 8);
     }
@@ -72,16 +65,11 @@ public class BiomeEffectsComponent extends BukkitComponent implements Listener, 
 
     private static class LocalConfiguration extends ConfigurationBase {
 
-        @Setting("enable-swamp")
-        public boolean enableSwamp = false;
         @Setting("enable-frozen")
         public boolean enableFrozen = false;
-        @Setting("secret-chest-location")
-        private String secretChestLocationString = "228,45,-232,City";
 
-        public Location secretChestLocation = LocationUtil.parseLocation(secretChestLocationString);
-        public String mainWorld = secretChestLocation.getWorld() != null
-                ? secretChestLocation.getWorld().getName() : "null";
+        @Setting("main-world")
+        public String mainWorld = "City";
     }
 
     @Override
@@ -90,7 +78,7 @@ public class BiomeEffectsComponent extends BukkitComponent implements Listener, 
         try {
             for (Player player : Bukkit.getWorld(config.mainWorld).getPlayers()) {
                 frostBite(player); // Attempt to execute Frost Bite
-                darkWind(player); // Attempt to execute the Dark Wind
+                //darkWind(player); // Attempt to execute the Dark Wind
 
             }
         } catch (Exception e) {
@@ -98,16 +86,11 @@ public class BiomeEffectsComponent extends BukkitComponent implements Listener, 
         }
     }
 
-    @EventHandler
-    public void onChestInteract(PlayerInteractEvent event) {
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockSpread(BlockSpreadEvent event) {
 
-        Block block = event.getClickedBlock();
-        Player player = event.getPlayer();
-        if (block != null && block.getTypeId() == BlockID.CHEST
-                && LocationUtil.isLocNearby(block.getLocation(), config.secretChestLocation, 1)) {
-            permission.playerAdd((World) null, player.getName(), "aurora.biomeeffects.override.darkwind");
-            ChatUtil.sendNotice(player, "");
-
+        if (event.getSource().getTypeId() == BlockID.MYCELIUM && event.getBlock().getBiome() != Biome.MUSHROOM_ISLAND) {
+            event.setCancelled(true);
         }
     }
 
@@ -140,8 +123,7 @@ public class BiomeEffectsComponent extends BukkitComponent implements Listener, 
     private void darkWind(Player player) {
 
         try {
-            if (config.enableSwamp
-                    && player.getLocation().getBlock().getBiome().equals(Biome.SWAMPLAND)) {
+            if (player.getLocation().getBlock().getBiome().equals(Biome.SWAMPLAND)) {
 
                 if (inst.hasPermission(player, "aurora.biomeeffects.override.darkwind")) {
                     player.removePotionEffect(PotionEffectType.BLINDNESS);
@@ -151,21 +133,11 @@ public class BiomeEffectsComponent extends BukkitComponent implements Listener, 
                     ChatUtil.sendWarning(player, "A dark wind passes over you.");
                     player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 600, 1));
                 }
-            } else if (config.enableSwamp && player.hasPotionEffect(PotionEffectType.BLINDNESS)) {
+            } else if (player.hasPotionEffect(PotionEffectType.BLINDNESS)) {
                 player.removePotionEffect(PotionEffectType.BLINDNESS);
             }
         } catch (Exception e) {
             log.warning("The Dark Wind could not be executed for the player: " + player.getName() + ".");
         }
-    }
-
-    private boolean setupPermissions() {
-
-        RegisteredServiceProvider<Permission> permissionProvider = server.getServicesManager().getRegistration(net
-                .milkbowl.vault.permission.Permission.class);
-        if (permissionProvider != null) {
-            permission = permissionProvider.getProvider();
-        }
-        return (permission != null);
     }
 }
