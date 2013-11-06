6140f5e1e18a2ebfef77f24f84b78c540ee7f88c
Dark Arc <theexcel@rocketmail.com>
2013-11-06 00:52:23 -0500

    Revised the inventory curse

--- src/main/java/com/skelril/aurora/prayer/PrayerFX/InventoryFX.java
+++ src/main/java/com/skelril/aurora/prayer/PrayerFX/InventoryFX.java
@@ -2,6 +2,8 @@ package com.skelril.aurora.prayer.PrayerFX;
 
 import com.sk89q.worldedit.blocks.BlockID;
 import com.skelril.aurora.prayer.PrayerType;
+import com.skelril.aurora.util.ChanceUtil;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -9,6 +11,20 @@ import org.bukkit.inventory.ItemStack;
  * Author: Turtle9598
  */
 public class InventoryFX extends AbstractPrayer {
+
+    int type, amount;
+
+    public InventoryFX() {
+        super();
+        this.type = BlockID.DIRT;
+        this.amount = 1;
+    }
+
+    public InventoryFX(int type, int amount) {
+        super();
+        this.type = type;
+        this.amount = amount;
+    }
 
     @Override
     public PrayerType getType() {
@@ -19,7 +35,13 @@ public class InventoryFX extends AbstractPrayer {
     @Override
     public void add(Player player) {
 
-        player.setItemInHand(new ItemStack(BlockID.DIRT, 1));
+        ItemStack held = player.getItemInHand().clone();
+        ItemStack stack = new ItemStack(type, ChanceUtil.getRandom(amount));
+        if (held != null && held.getTypeId() != BlockID.AIR && !held.isSimilar(stack)) {
+            Item item = player.getWorld().dropItem(player.getLocation(), held);
+            item.setPickupDelay(20 * 5);
+        }
+        player.setItemInHand(stack);
     }
 
     @Override
