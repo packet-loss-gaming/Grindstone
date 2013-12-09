144053ca5c033165ec87287303aa33e22d8155a0
Dark Arc <theexcel@rocketmail.com>
2013-12-09 12:43:16 -0500

    Changed how the AFK system determines whether to keep god mode or not

--- src/main/java/com/skelril/aurora/IdleComponent.java
+++ src/main/java/com/skelril/aurora/IdleComponent.java
@@ -2,8 +2,8 @@ package com.skelril.aurora;
 
 import com.sk89q.commandbook.CommandBook;
 import com.sk89q.commandbook.GodComponent;
-import com.skelril.aurora.admin.AdminComponent;
-import com.skelril.aurora.admin.AdminState;
+import com.sk89q.commandbook.session.PersistentSession;
+import com.sk89q.commandbook.session.SessionComponent;
 import com.skelril.aurora.util.ChatUtil;
 import com.skelril.aurora.util.LocationUtil;
 import com.zachsthings.libcomponents.ComponentInformation;
@@ -14,6 +14,7 @@ import com.zachsthings.libcomponents.config.ConfigurationBase;
 import com.zachsthings.libcomponents.config.Setting;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,28 +25,24 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.*;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Author: Turtle9598
  */
 @ComponentInformation(friendlyName = "AFK Checker", desc = "AFK Checking.")
-@Depend(components = {GodComponent.class, AdminState.class})
+@Depend(components = {GodComponent.class, SessionComponent.class})
 public class IdleComponent extends BukkitComponent implements Runnable, Listener {
 
     private final CommandBook inst = CommandBook.inst();
     private final Server server = CommandBook.server();
 
     @InjectComponent
-    AdminComponent adminComponent;
-
+    private GodComponent godComponent;
     @InjectComponent
-    GodComponent godComponent;
+    private SessionComponent sessions;
 
     private LocalConfiguration config;
-    private ConcurrentHashMap<Player, Long> afk = new ConcurrentHashMap<>();
 
     @Override
     public void enable() {
@@ -77,59 +74,71 @@ public class IdleComponent extends BukkitComponent implements Runnable, Listener
 
     public boolean isAfk(Player player) {
 
-        return isAfk(afk.get(player));
+        return isAfk(sessions.getSession(AFKSession.class, player).getLastUpdate());
     }
 
     public boolean isAfk(long time) {
 
-        return System.currentTimeMillis() - time >= TimeUnit.MINUTES.toMillis(config.afkMinutes);
+        return time != 0 && System.currentTimeMillis() - time >= TimeUnit.MINUTES.toMillis(config.afkMinutes);
     }
 
     public boolean shouldKick(Player player) {
 
-        return shouldKick(afk.get(player));
+        return shouldKick(sessions.getSession(AFKSession.class, player).getLastUpdate());
     }
 
     public boolean shouldKick(long time) {
 
-        double fraction = (server.getMaxPlayers() - server.getOnlinePlayers().length) / (double) server.getMaxPlayers();
-        int duration = (int) Math.max(config.afkMinutes + 2, config.afkKickMinutes * fraction);
-        return System.currentTimeMillis() - time >= TimeUnit.MINUTES.toMillis(duration);
+        double maxP = server.getMaxPlayers();
+        double curP = server.getOnlinePlayers().length;
+
+        double fraction = ((maxP - curP) + maxP * .2) / maxP;
+        int duration = (int) Math.max(config.afkMinutes + 2, Math.min(config.afkKickMinutes, config.afkKickMinutes * fraction));
+        return time != 0 && System.currentTimeMillis() - time >= TimeUnit.MINUTES.toMillis(duration);
     }
 
     @Override
     public void run() {
 
-        for (final Map.Entry<Player, Long> entry : afk.entrySet()) {
+        for (final AFKSession session : sessions.getSessions(AFKSession.class).values()) {
 
-            if (isAfk(entry.getValue())) {
-                if (shouldKick(entry.getValue())) {
-                    LocationUtil.toGround(entry.getKey());
-                    adminComponent.deadmin(entry.getKey(), true);
-                    entry.getKey().setSleepingIgnored(false);
+            if (session == null) continue;
+            final Player target = session.getPlayer();
+            if (target == null || !session.getPlayer().isValid()) continue;
+
+            if (isAfk(session.getLastUpdate())) {
+                if (shouldKick(session.getLastUpdate())) {
+                    target.setSleepingIgnored(false);
 
                     server.getScheduler().runTaskLater(inst, new Runnable() {
 
                         @Override
                         public void run() {
 
-                            entry.getKey().kickPlayer("Inactivity - " + (System.currentTimeMillis() - entry.getValue()) / 60000 + " Minutes");
+                            target.kickPlayer("Inactivity - " + (System.currentTimeMillis() - session.getLastUpdate()) / 60000 + " Minutes");
                         }
                     }, 1);
-                } else if (!entry.getKey().isSleepingIgnored()) {
-                    String name = entry.getKey().getName();
-                    entry.getKey().setPlayerListName(ChatColor.GRAY + name.substring(0, Math.min(14, name.length())));
-                    entry.getKey().setSleepingIgnored(true);
-                    godComponent.enableGodMode(entry.getKey());
-                    ChatUtil.sendNotice(entry.getKey(), "You are now marked as AFK.");
+                } else if (!target.isSleepingIgnored()) {
+                    String name = target.getName();
+                    target.setPlayerListName(ChatColor.GRAY + name.substring(0, Math.min(14, name.length())));
+                    target.setSleepingIgnored(true);
+                    if (!godComponent.hasGodMode(target)) {
+                        godComponent.enableGodMode(target);
+                        session.setAppliedGodMode(true);
+                    } else {
+                        session.setAppliedGodMode(false);
+                    }
+                    ChatUtil.sendNotice(target, "You are now marked as AFK.");
                 }
 
-            } else if (entry.getKey().isSleepingIgnored()) {
+            } else if (target.isSleepingIgnored()) {
 
-                entry.getKey().setPlayerListName(entry.getKey().getName());
-                entry.getKey().setSleepingIgnored(false);
-                if (!adminComponent.isAdmin(entry.getKey())) godComponent.disableGodMode(entry.getKey());
-                ChatUtil.sendNotice(entry.getKey(), "You are no longer marked as AFK.");
+                target.setPlayerListName(target.getName());
+                target.setSleepingIgnored(false);
+                if (session.appliedGodMode()) {
+                    godComponent.disableGodMode(target);
+                }
+                ChatUtil.sendNotice(target, "You are no longer marked as AFK.");
             }
         }
     }
@@ -137,7 +146,7 @@ public class IdleComponent extends BukkitComponent implements Runnable, Listener
     @EventHandler
     public void onEntityTargetPlayer(EntityTargetEvent event) {
 
-        if (event.getTarget() instanceof Player && afk.containsKey(event.getTarget())) {
+        if (event.getTarget() instanceof Player) {
 
             if (isAfk((Player) event.getTarget())) {
                 event.setCancelled(true);
@@ -148,7 +157,7 @@ public class IdleComponent extends BukkitComponent implements Runnable, Listener
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
 
-        afk.put(event.getPlayer(), System.currentTimeMillis());
+        update(event.getPlayer());
     }
 
     @EventHandler
@@ -208,12 +217,6 @@ public class IdleComponent extends BukkitComponent implements Runnable, Listener
         if (player.isSneaking() ? distanceSQ > config.sneakMovementThreshold : distanceSQ > config.movementThreshold) update(player);
     }
 
-    @EventHandler
-    public void onPlayerDisconnect(PlayerQuitEvent event) {
-
-        if (afk.containsKey(event.getPlayer())) afk.remove(event.getPlayer());
-    }
-
     /**
      * This method is used to update a player's last active time
      *
@@ -221,10 +224,51 @@ public class IdleComponent extends BukkitComponent implements Runnable, Listener
      */
     public void update(Player player) {
 
-        afk.put(player, System.currentTimeMillis());
+        AFKSession session = sessions.getSession(AFKSession.class, player);
+        session.setLastUpdate(System.currentTimeMillis());
 
-        if (godComponent.hasGodMode(player) && !adminComponent.isAdmin(player)) {
+        if (godComponent.hasGodMode(player) && session.appliedGodMode()) {
             godComponent.disableGodMode(player);
+        }
+    }
+
+    // AFK Session
+    private static class AFKSession extends PersistentSession {
+
+        public static final long MAX_AGE = TimeUnit.MINUTES.toMillis(30);
+
+        private long lastUpdate = 0;
+        private boolean appliedGodMode = true;
+
+        protected AFKSession() {
+
+            super(MAX_AGE);
+        }
+
+        public Player getPlayer() {
+
+            CommandSender sender = super.getOwner();
+            return sender instanceof Player ? (Player) sender : null;
+        }
+
+        public long getLastUpdate() {
+
+            return lastUpdate;
+        }
+
+        public void setLastUpdate(long lastUpdate) {
+
+            this.lastUpdate = lastUpdate;
+        }
+
+        public boolean appliedGodMode() {
+
+            return appliedGodMode;
+        }
+
+        public void setAppliedGodMode(boolean appliedGodMode) {
+
+            this.appliedGodMode = appliedGodMode;
         }
     }
 }
