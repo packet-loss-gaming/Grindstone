bf7eec00a4db0441626fe6f750ee501608955862
Dark Arc <theexcel@rocketmail.com>
2013-12-22 00:19:03 -0500

    Updated the Shutdown system

--- src/main/java/com/skelril/aurora/admin/ShutdownComponent.java
+++ src/main/java/com/skelril/aurora/admin/ShutdownComponent.java
@@ -1,6 +1,7 @@
 package com.skelril.aurora.admin;
 
 import com.sk89q.commandbook.CommandBook;
+import com.sk89q.commandbook.CommandBookUtil;
 import com.sk89q.minecraft.util.commands.Command;
 import com.sk89q.minecraft.util.commands.CommandContext;
 import com.sk89q.minecraft.util.commands.CommandException;
@@ -16,6 +17,9 @@ import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 /**
@@ -39,28 +43,34 @@ public class ShutdownComponent extends BukkitComponent {
 
     public class Commands {
 
-        @Command(aliases = {"restart", "kds"}, usage = "[time]", desc = "Used to restart the server", min = 0, max = 1)
+        @Command(aliases = {"shutdown"}, usage = "[time] [excepted downtime]",
+                desc = "Used to restart the server", min = 0, max = 2)
         @CommandPermissions({"aurora.admin.server.shutdown"})
         public void shutdownCmd(CommandContext args, CommandSender sender) throws CommandException {
 
-            int delay;
-            if (args.argsLength() == 0) {
-                delay = 60;
-            } else {
+            int delay = 60;
+            long expectedDowntime = TimeUnit.SECONDS.toMillis(30);
+            if (args.argsLength() > 0) {
                 try {
                     delay = Math.min(120, Math.max(10, Integer.parseInt(args.getString(0))));
+                    if (args.argsLength() > 1) {
+                        expectedDowntime = CommandBookUtil.matchDate(args.getString(1));
+                    }
                 } catch (NumberFormatException ex) {
                     throw new CommandException("Invalid time entered!");
                 }
             }
-            shutdown(delay);
+            shutdown(delay, expectedDowntime);
         }
     }
 
     private int seconds = 0;
+    private long downTime = 0;
     private BukkitTask task = null;
 
-    public void shutdown(final int assignedSeconds) {
+    public void shutdown(final int assignedSeconds, long expectedDownTime) {
+
+        this.downTime = expectedDownTime;
 
         if (assignedSeconds > 0) {
             this.seconds = assignedSeconds;
@@ -70,6 +80,9 @@ public class ShutdownComponent extends BukkitComponent {
         }
 
         if (task != null) return;
+
+        final NumberFormat formatter = new DecimalFormat("#");
+
         // New Task
         task = inst.getServer().getScheduler().runTaskTimer(inst, new Runnable() {
 
@@ -78,9 +91,10 @@ public class ShutdownComponent extends BukkitComponent {
 
                 server.getPluginManager().callEvent(new ServerShutdownEvent(seconds));
                 if (seconds > 0 && seconds % 5 == 0 || seconds <= 10 && seconds > 0) {
-                    Bukkit.broadcastMessage(ChatColor.RED + "Restarting in " + seconds + " seconds!");
+                    Bukkit.broadcastMessage(ChatColor.RED + "Shutting down in " + seconds + " seconds - for "
+                            + formatter.format(downTime / 1000) + " seconds of downtime!");
                 } else if (seconds < 1) {
-                    Bukkit.broadcastMessage(ChatColor.RED + "Restarting!");
+                    Bukkit.broadcastMessage(ChatColor.RED + "Shutting down!");
                     server.shutdown();
                     return;
                 }
