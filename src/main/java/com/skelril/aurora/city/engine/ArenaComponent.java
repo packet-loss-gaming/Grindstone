64ce0050112131f897f6d1feb4911b05a6000226
Dark Arc <theexcel@rocketmail.com>
2014-04-29 01:03:47 -0400

    Initial commit of Patient X boss

--- src/main/java/com/skelril/aurora/city/engine/ArenaComponent.java
+++ src/main/java/com/skelril/aurora/city/engine/ArenaComponent.java
@@ -312,6 +312,9 @@ public class ArenaComponent extends BukkitComponent implements Listener, Runnabl
                     e.printStackTrace();
                 }
             }
+
+            // Add Patient X
+            arenas.add(new PatientXArena(world, mgr.get(world).getRegion("glacies-mare-district-mad-man"), adminComponent));
         }
     }
 
