0b541da4b3613591e019cdda937abb43422b0a3c
Dark Arc <theexcel@rocketmail.com>
2014-05-20 00:13:27 -0400

    Added a hierarchy to the AdminState enum

--- src/main/java/com/skelril/aurora/admin/AdminState.java
+++ src/main/java/com/skelril/aurora/admin/AdminState.java
@@ -6,25 +6,37 @@
 
 package com.skelril.aurora.admin;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Author: Turtle9598
  */
 public enum AdminState {
+    MEMBER,
+    MODERATOR(MEMBER),
+    ADMIN(MODERATOR),
+    SYSOP(ADMIN);
 
-    /**
-     * Elevated Shade Admin Level
-     */
-    SYSOP,
-    /**
-     * Lord and Shade Admin Level
-     */
-    ADMIN,
-    /**
-     * Moderator Admin Level
-     */
-    MODERATOR,
-    /**
-     * General User State
-     */
-    MEMBER
+    private final AdminState child;
+    private Set<AdminState> states = new HashSet<>();
+
+    AdminState() {
+        child = null;
+    }
+
+    AdminState(AdminState child) {
+        this.child = child;
+        addState(child);
+    }
+
+    private void addState(AdminState state) {
+        if (state == null) return;
+        states.add(state);
+        addState(state.child);
+    }
+
+    public boolean isAbove(AdminState state) {
+        return this.equals(state) || states.contains(state);
+    }
 }
