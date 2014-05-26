11d52712f73534326aabde8403f4a9a28e1618c0
Dark Arc <theexcel@rocketmail.com>
2014-05-26 04:39:38 -0400

    Used nested config for Patient X

--- src/main/java/com/skelril/aurora/city/engine/area/areas/PatientX/PatientXConfig.java
+++ src/main/java/com/skelril/aurora/city/engine/area/areas/PatientX/PatientXConfig.java
@@ -10,22 +10,22 @@ import com.zachsthings.libcomponents.config.ConfigurationBase;
 import com.zachsthings.libcomponents.config.Setting;
 
 public class PatientXConfig extends ConfigurationBase {
-    @Setting("boss-health")
+    @Setting("boss.health")
     public int bossHealth = 1000;
-    @Setting("base-boss-hit")
+    @Setting("boss.base-hit")
     public double baseBossHit = 175;
-    @Setting("ice-chance")
+    @Setting("ice.chance")
     public int iceChance = 12;
-    @Setting("ice-explosive-change-chance")
+    @Setting("ice.explosive-chance")
     public int iceChangeChance = 30;
     @Setting("snow-ball-chance")
     public int snowBallChance = 10;
     @Setting("player-value")
     public int playerVal = 50000;
-    @Setting("default-difficulty")
+    @Setting("difficulty.default")
     public int defaultDifficulty = 3;
-    @Setting("min-difficulty")
+    @Setting("difficulty.min")
     public int minDifficulty = 3;
-    @Setting("max-difficulty")
+    @Setting("difficulty.max")
     public int maxDifficulty = 9;
 }
