07a16bcf9936da7b9d1d39f8041e2600928cdd00
Wyatt Childers <theexcel@rocketmail.com>
2014-06-30 21:11:14 -0400

    Improved the flexibility of Timed Runnable

--- src/main/java/com/skelril/aurora/util/timer/TimedRunnable.java
+++ src/main/java/com/skelril/aurora/util/timer/TimedRunnable.java
@@ -14,26 +14,35 @@ public class TimedRunnable implements Runnable {
     private IntegratedRunnable action;
 
     private int times;
+    private boolean done = false;
 
     public TimedRunnable(IntegratedRunnable action, int times) {
-
         this.action = action;
         this.times = times;
     }
 
-    public void addTime(int times) {
+    public boolean isComplete() {
+        return done;
+    }
 
+    public void addTime(int times) {
         this.times += times;
     }
 
-    public void setTask(BukkitTask task) {
+    public void setTimes(int times) {
+        this.times = times;
+    }
 
+    public int getTimes() {
+        return times;
+    }
+
+    public void setTask(BukkitTask task) {
         this.task = task;
     }
 
     @Override
     public void run() {
-
         if (times > 0) {
             if (action.run(times)) {
                 times--;
@@ -44,13 +53,15 @@ public class TimedRunnable implements Runnable {
     }
 
     public void cancel() {
-
         cancel(false);
     }
 
     public void cancel(boolean withEnd) {
 
+        if (done) return; // Task is done
+
         if (withEnd) action.end();
         task.cancel();
+        done = true;
     }
 }
