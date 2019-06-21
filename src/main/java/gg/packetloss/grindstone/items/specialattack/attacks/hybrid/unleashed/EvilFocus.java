e9f58684e431a9f85329436520c3b4db27f16140
Wyatt Childers <wchilders@nearce.com>
2019-06-20 21:01:05 -0400

    Change how evil focus locks the player in place

--- src/main/java/gg/packetloss/grindstone/items/specialattack/attacks/hybrid/unleashed/EvilFocus.java
+++ src/main/java/gg/packetloss/grindstone/items/specialattack/attacks/hybrid/unleashed/EvilFocus.java
@@ -9,11 +9,16 @@ package gg.packetloss.grindstone.items.specialattack.attacks.hybrid.unleashed;
 import gg.packetloss.grindstone.items.specialattack.EntityAttack;
 import gg.packetloss.grindstone.items.specialattack.attacks.melee.MeleeSpecial;
 import gg.packetloss.grindstone.items.specialattack.attacks.ranged.RangedSpecial;
+import gg.packetloss.grindstone.util.ChanceUtil;
+import gg.packetloss.grindstone.util.timer.IntegratedRunnable;
+import gg.packetloss.grindstone.util.timer.TimedRunnable;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitTask;
 
 public class EvilFocus extends EntityAttack implements MeleeSpecial, RangedSpecial {
 
@@ -21,13 +26,35 @@ public class EvilFocus extends EntityAttack implements MeleeSpecial, RangedSpeci
         super(owner, target);
     }
 
+    private void lockPosition() {
+        Location lockedLocation = target.getLocation();
+
+        IntegratedRunnable freeze = new IntegratedRunnable() {
+            @Override
+            public boolean run(int times) {
+                if (owner.hasLineOfSight(target) && target.isValid()) {
+                    lockedLocation.setDirection(target.getEyeLocation().getDirection()).setDirection(target.getEyeLocation().getDirection());
+                    target.teleport(lockedLocation, PlayerTeleportEvent.TeleportCause.UNKNOWN);
+                }
+
+                return true;
+            }
+
+            @Override
+            public void end() {
+            }
+        };
+
+        TimedRunnable freezeTask = new TimedRunnable(freeze, ChanceUtil.getRangedRandom(3, 8));
+        BukkitTask freezeTaskExecutor = server.getScheduler().runTaskTimer(inst, freezeTask, 20 * 2, 20);
+        freezeTask.setTask(freezeTaskExecutor);
+    }
+
     @Override
     public void activate() {
+        target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, (int) (10 * target.getHealth()), 1), true);
+        lockPosition();
 
-        target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, (int) (10 * target.getHealth()), 9), true);
-        if (target instanceof Player) {
-            target.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 4, 0), true);
-        }
         target.getWorld().playSound(target.getLocation(), Sound.ENTITY_GHAST_SCREAM, 1, .02F);
         inform("Your weapon traps your foe in their own sins.");
     }
