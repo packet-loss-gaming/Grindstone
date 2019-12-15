eee1f4b05960be6f81304c89b246763a8217b9fb
Wyatt Childers <wchilders@nearce.com>
2019-12-14 19:02:35 -0500

    Add shulker boxes to container box

--- src/main/java/gg/packetloss/grindstone/util/EnvironmentUtil.java
+++ src/main/java/gg/packetloss/grindstone/util/EnvironmentUtil.java
@@ -148,7 +148,13 @@ public class EnvironmentUtil {
 
     private static final Set<Material> containerBlocks = Set.of(
             Material.BREWING_STAND, Material.CHEST, Material.DISPENSER, Material.DROPPER, Material.FURNACE,
-            Material.BURNING_FURNACE, Material.JUKEBOX, Material.ENDER_CHEST, Material.TRAPPED_CHEST, Material.HOPPER
+            Material.BURNING_FURNACE, Material.JUKEBOX, Material.ENDER_CHEST, Material.TRAPPED_CHEST, Material.HOPPER,
+            Material.WHITE_SHULKER_BOX, Material.ORANGE_SHULKER_BOX, Material.MAGENTA_SHULKER_BOX,
+            Material.LIGHT_BLUE_SHULKER_BOX, Material.YELLOW_SHULKER_BOX, Material.LIME_SHULKER_BOX,
+            Material.PINK_SHULKER_BOX, Material.GRAY_SHULKER_BOX, Material.SILVER_SHULKER_BOX,
+            Material.CYAN_SHULKER_BOX, Material.PURPLE_SHULKER_BOX, Material.BLUE_SHULKER_BOX,
+            Material.BROWN_SHULKER_BOX, Material.GREEN_SHULKER_BOX,
+            Material.RED_SHULKER_BOX, Material.BLACK_SHULKER_BOX
     );
 
     public static boolean isContainer(Block block) {
