2db092cb10d9155ec14e438f619437ed529b9cf3
Wyatt Childers <wchilders@nearce.com>
2020-01-11 19:47:38 -0500

    Improve portal generator to be more flexible and efficient

--- src/main/java/gg/packetloss/grindstone/portal/PortalGenerator.java
+++ src/main/java/gg/packetloss/grindstone/portal/PortalGenerator.java
@@ -25,11 +25,6 @@ public class PortalGenerator {
     }
 
     private boolean tryWalk(BlockFace direction) {
-        PortalTester tester = new PortalTester(direction);
-        if (!tester.walk()) {
-            return false;
-        }
-
         PortalFiller filler = new PortalFiller(direction);
         if (!filler.checkFill()) {
             return false;
@@ -54,125 +49,6 @@ public class PortalGenerator {
         return false;
     }
 
-    class PortalTester {
-        private BlockFace walkingDirection;
-        private Block currentBlock;
-
-        private int step = 0;
-
-        private int minVert;
-        private int maxVert;
-        private int minHoriz;
-        private int maxHoriz;
-
-        private void initHoriz() {
-            switch (walkingDirection) {
-                case NORTH:
-                    minHoriz = maxHoriz = ignitedBlock.getZ();
-                    break;
-                case EAST:
-                    minHoriz = maxHoriz = ignitedBlock.getX();
-                    break;
-            }
-        }
-
-        private void initVert() {
-            this.minVert = this.maxVert = ignitedBlock.getY();
-        }
-
-        public PortalTester(BlockFace walkingDirection) {
-            this.walkingDirection = walkingDirection;
-
-            this.initHoriz();
-            this.initVert();
-        }
-
-        private void updateHoriz(Block block) {
-            switch (walkingDirection) {
-                case NORTH:
-                    minHoriz = Math.min(minHoriz, block.getZ());
-                    maxHoriz = Math.max(maxHoriz, block.getZ());
-                    break;
-                case EAST:
-                    minHoriz = Math.min(minHoriz, block.getX());
-                    maxHoriz = Math.max(maxHoriz, block.getX());
-                    break;
-            }
-        }
-
-        private void updateVert(Block block) {
-            minVert = Math.min(minVert, block.getY());
-            maxVert = Math.max(maxVert, block.getY());
-        }
-
-        private void walk(Block block) {
-            this.updateHoriz(block);
-            this.updateVert(block);
-
-            this.currentBlock = block;
-        }
-
-        private boolean trialWalk(BlockFace direction, BlockFace adjustment) {
-            Block testBlock = currentBlock.getRelative(direction).getRelative(adjustment);
-            if (testBlock.getType() == blockType) {
-                walk(testBlock);
-                return true;
-            }
-
-            return false;
-        }
-
-        public boolean walk() {
-            Block startingBlock = ignitedBlock.getRelative(BlockFace.DOWN);
-
-            currentBlock = startingBlock;
-            while (!exceedsMaxStep() && !exceedsHorizLimit() && !exceedsVertLimit()) {
-                if (step == 3 && currentBlock.equals(startingBlock)) {
-                    return meetsHorizMin() && meetsVertMin();
-                }
-
-                BlockFace verticalDirection = step < 2 ? BlockFace.UP : BlockFace.DOWN;
-                BlockFace horizontalDirection = step == 0 || step == 3 ? walkingDirection : walkingDirection.getOppositeFace();
-
-                if (trialWalk(verticalDirection, BlockFace.SELF)) {
-                    continue;
-                }
-
-                if (trialWalk(horizontalDirection, verticalDirection)) {
-                    continue;
-                }
-
-                if (trialWalk(horizontalDirection, BlockFace.SELF)) {
-                    continue;
-                }
-
-                ++step;
-            }
-
-            return false;
-        }
-
-        public boolean meetsVertMin() {
-            return (maxHoriz + 1) - minHoriz >= 4;
-        }
-
-        public boolean meetsHorizMin() {
-            return (maxVert + 1) - minVert >= 5;
-        }
-
-        public boolean exceedsVertLimit() {
-            return (maxHoriz + 1) - minHoriz > 23;
-        }
-
-        public boolean exceedsHorizLimit() {
-            return (maxVert + 1) - minVert > 23;
-        }
-
-        public boolean exceedsMaxStep() {
-            return step > 3;
-        }
-    }
-
     private boolean isAirOrFire(Block block) {
         Material blockType = block.getType();
         return blockType == Material.AIR || blockType == Material.FIRE;
@@ -180,6 +56,7 @@ public class PortalGenerator {
 
     class PortalFiller {
         private BlockFace walkingDirection;
+        private Set<BlockVector3> blocks;
 
         public PortalFiller(BlockFace walkingDirection) {
             this.walkingDirection = walkingDirection;
@@ -188,11 +65,13 @@ public class PortalGenerator {
         private boolean doFillOp(Block sourceBlock, Predicate<Block> op) {
             switch (walkingDirection) {
                 case NORTH:
-                    return op.test(sourceBlock.getRelative(BlockFace.NORTH)) &&
+                    return op.test(sourceBlock.getRelative(BlockFace.DOWN)) &&
+                            op.test(sourceBlock.getRelative(BlockFace.NORTH)) &&
                             op.test(sourceBlock.getRelative(BlockFace.SOUTH)) &&
                             op.test(sourceBlock.getRelative(BlockFace.UP));
                 case EAST:
-                    return op.test(sourceBlock.getRelative(BlockFace.EAST)) &&
+                    return op.test(sourceBlock.getRelative(BlockFace.DOWN)) &&
+                            op.test(sourceBlock.getRelative(BlockFace.EAST)) &&
                             op.test(sourceBlock.getRelative(BlockFace.WEST)) &&
                             op.test(sourceBlock.getRelative(BlockFace.UP));
             }
@@ -200,10 +79,10 @@ public class PortalGenerator {
             throw new UnsupportedOperationException();
         }
 
-        private boolean checkFill(Set<BlockVector3> alreadyChecked, Block block) {
+        private boolean checkFill(Block block) {
             return doFillOp(block, (opBlock) -> {
                 BlockVector3 blockCoords = WorldEditBridge.toBlockVec3(opBlock);
-                if (alreadyChecked.contains(blockCoords)) {
+                if (blocks.contains(blockCoords)) {
                     return true;
                 }
 
@@ -219,25 +98,25 @@ public class PortalGenerator {
                     return false;
                 }
 
-                alreadyChecked.add(blockCoords);
+                blocks.add(blockCoords);
+                if (blocks.size() > Math.pow(22, 2)) {
+                    return false;
+                }
 
-                return checkFill(alreadyChecked, opBlock);
+                return checkFill(opBlock);
             });
         }
 
         public boolean checkFill() {
-            return checkFill(new HashSet<>(), ignitedBlock);
+            blocks = new HashSet<>();
+            return checkFill(ignitedBlock);
         }
 
-        private boolean fill(Block block) {
-            return doFillOp(block, (opBlock) -> {
-                Material opBlockType = opBlock.getType();
-                if (opBlockType == blockType || opBlockType == Material.NETHER_PORTAL) {
-                    return true;
-                }
-
-                opBlock.setType(Material.NETHER_PORTAL);
-                Orientable blockData = ((Orientable) opBlock.getBlockData());
+        public void fill() {
+            blocks.forEach((pos) -> {
+                Block block = ignitedBlock.getWorld().getBlockAt(pos.getX(), pos.getY(), pos.getZ());
+                block.setType(Material.NETHER_PORTAL);
+                Orientable blockData = ((Orientable) block.getBlockData());
                 switch (walkingDirection) {
                     case NORTH:
                         blockData.setAxis(Axis.Z);
@@ -246,14 +125,8 @@ public class PortalGenerator {
                         blockData.setAxis(Axis.X);
                         break;
                 }
-                opBlock.setBlockData(blockData);
-
-                return fill(opBlock);
+                block.setBlockData(blockData);
             });
-        }
-
-        public void fill() {
-            fill(ignitedBlock);
         }
     }
 }
