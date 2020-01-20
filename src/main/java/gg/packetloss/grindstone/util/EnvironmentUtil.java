ed9a252466fd297c721d2d6b76fc2e70055469c3
Wyatt Childers <wchilders@nearce.com>
2020-01-20 17:17:45 -0500

    Fixed a bug where shulker boxes in block form were not considered containers

--- src/main/java/gg/packetloss/grindstone/util/EnvironmentUtil.java
+++ src/main/java/gg/packetloss/grindstone/util/EnvironmentUtil.java
@@ -168,11 +168,14 @@ public class EnvironmentUtil {
     private static final Set<Material> containerBlocks = Set.of(
             Material.BREWING_STAND, Material.CHEST, Material.DISPENSER, Material.DROPPER, Material.FURNACE,
             Material.JUKEBOX, Material.ENDER_CHEST, Material.TRAPPED_CHEST, Material.HOPPER,
-            Material.WHITE_SHULKER_BOX, Material.ORANGE_SHULKER_BOX, Material.MAGENTA_SHULKER_BOX,
-            Material.LIGHT_BLUE_SHULKER_BOX, Material.YELLOW_SHULKER_BOX, Material.LIME_SHULKER_BOX,
-            Material.PINK_SHULKER_BOX, Material.GRAY_SHULKER_BOX, Material.LIGHT_GRAY_SHULKER_BOX,
-            Material.CYAN_SHULKER_BOX, Material.PURPLE_SHULKER_BOX, Material.BLUE_SHULKER_BOX,
-            Material.BROWN_SHULKER_BOX, Material.GREEN_SHULKER_BOX,
+            Material.SHULKER_BOX,
+
+            // These are only valid in the inventory
+            Material.WHITE_SHULKER_BOX, Material.ORANGE_SHULKER_BOX,
+            Material.MAGENTA_SHULKER_BOX, Material.LIGHT_BLUE_SHULKER_BOX, Material.YELLOW_SHULKER_BOX,
+            Material.LIME_SHULKER_BOX, Material.PINK_SHULKER_BOX, Material.GRAY_SHULKER_BOX,
+            Material.LIGHT_GRAY_SHULKER_BOX, Material.CYAN_SHULKER_BOX, Material.PURPLE_SHULKER_BOX,
+            Material.BLUE_SHULKER_BOX, Material.BROWN_SHULKER_BOX, Material.GREEN_SHULKER_BOX,
             Material.RED_SHULKER_BOX, Material.BLACK_SHULKER_BOX
     );
 
