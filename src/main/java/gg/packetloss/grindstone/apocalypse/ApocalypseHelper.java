3040dcdc8d5d2f96ade499a50fb7558b4cc09fe6
Wyatt Childers <wchilders@nearce.com>
2020-02-24 19:20:50 -0500

    Make sure drop suppression is reverted regardless of exceptions

--- src/main/java/gg/packetloss/grindstone/apocalypse/ApocalypseHelper.java
+++ src/main/java/gg/packetloss/grindstone/apocalypse/ApocalypseHelper.java
@@ -60,7 +60,10 @@ public class ApocalypseHelper {
         boolean prevValue = suppressDrops;
 
         suppressDrops = true;
-        op.run();
-        suppressDrops = prevValue;
+        try {
+            op.run();
+        } finally {
+            suppressDrops = prevValue;
+        }
     }
 }
