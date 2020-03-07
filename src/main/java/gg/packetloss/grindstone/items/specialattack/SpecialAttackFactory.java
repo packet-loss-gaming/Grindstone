780380aa8bac379993b12a14e61ee3a19778e9b4
Wyatt Childers <wchilders@nearce.com>
2020-03-06 20:35:30 -0500

    Don't run special attack damage when the defender is invulnerable

--- src/main/java/gg/packetloss/grindstone/items/specialattack/SpecialAttackFactory.java
+++ src/main/java/gg/packetloss/grindstone/items/specialattack/SpecialAttackFactory.java
@@ -27,7 +27,7 @@ public class SpecialAttackFactory {
 
     public static boolean processDamage(LivingEntity attacker, LivingEntity defender,
                                         SpecialAttack spec, double amount) {
-        if (defender.isDead()) {
+        if (defender.isDead() || defender.getNoDamageTicks() > 0) {
             return false;
         }
 
