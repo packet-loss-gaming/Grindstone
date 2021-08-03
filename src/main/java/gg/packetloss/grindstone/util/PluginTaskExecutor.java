efd225ec9b22c02a2450503c97a633a302e06ae9
Wyatt Childers <wchilders@nearce.com>
2021-08-03 17:48:12 -0400

    Add proper background job shutdown

--- src/main/java/gg/packetloss/grindstone/util/PluginTaskExecutor.java
+++ src/main/java/gg/packetloss/grindstone/util/PluginTaskExecutor.java
@@ -6,13 +6,32 @@
 
 package gg.packetloss.grindstone.util;
 
+import com.sk89q.commandbook.CommandBook;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PluginTaskExecutor {
     private static ExecutorService INSTANCE = Executors.newCachedThreadPool();
 
     public static void submitAsync(Runnable task) {
         INSTANCE.submit(task);
+    }
+
+    public static void shutdown() throws InterruptedException {
+        INSTANCE.shutdown();
+
+        if (INSTANCE.awaitTermination(5, TimeUnit.SECONDS)) {
+            return;
+        }
+
+        CommandBook.logger().info("Waiting for background jobs...");
+        if (INSTANCE.awaitTermination(30, TimeUnit.SECONDS)) {
+            return;
+        }
+
+        int numTasksCancelled = INSTANCE.shutdownNow().size();
+        CommandBook.logger().severe(numTasksCancelled + " background jobs timed out.");
     }
 }
