8b300dd1673d6e1295d56a242b8d16e071b0f21b
Wyatt Childers <wchilders@nearce.com>
2021-08-10 21:40:18 -0400

    Add a way to shutdown the server at a convenient time (i.e. when it remains idle for a while).

--- src/main/java/gg/packetloss/grindstone/admin/ShutdownCommands.java
+++ src/main/java/gg/packetloss/grindstone/admin/ShutdownCommands.java
@@ -9,6 +9,7 @@ package gg.packetloss.grindstone.admin;
 import com.google.common.base.Joiner;
 import com.sk89q.worldedit.command.util.CommandPermissions;
 import com.sk89q.worldedit.command.util.CommandPermissionsConditionGenerator;
+import gg.packetloss.grindstone.util.ChatUtil;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.enginehub.piston.annotation.Command;
@@ -19,7 +20,7 @@ import java.util.List;
 
 @CommandContainer(superTypes = CommandPermissionsConditionGenerator.Registration.class)
 public class ShutdownCommands {
-    private ShutdownComponent component;
+    private final ShutdownComponent component;
 
     public ShutdownCommands(ShutdownComponent component) {
         this.component = component;
@@ -38,5 +39,12 @@ public class ShutdownCommands {
         }
 
         component.shutdown(sender instanceof Player ? (Player) sender : null, delay, expectedDowntime);
+    }
+
+    @Command(name = "idleshutdown", desc = "Used to restart the server when all players have disconnected")
+    @CommandPermissions({"aurora.admin.server.shutdown"})
+    public void idleShutdownCmd(CommandSender sender) {
+        component.idleShutdown();
+        ChatUtil.sendNotice(sender, "The server will shutdown 30 seconds after all players have stayed disconnected.");
     }
 }
