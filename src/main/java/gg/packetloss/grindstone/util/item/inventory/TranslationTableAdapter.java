3a38a5b9cb7a52c517b2760d08e9cc936aab9f4c
Wyatt Childers <wchilders@nearce.com>
2021-08-14 19:46:21 -0400

    Use a precomputed table for storage adapters

---
+++ src/main/java/gg/packetloss/grindstone/util/item/inventory/TranslationTableAdapter.java
@@ -0,0 +1,95 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package gg.packetloss.grindstone.util.item.inventory;
+
+import gg.packetloss.grindstone.util.item.ItemUtil;
+import org.apache.commons.lang.Validate;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class TranslationTableAdapter implements InventoryAdapter {
+    private final int[] translationTable;
+
+    private final Player player;
+    private final PlayerInventory playerInventory;
+
+    private final int length;
+    private final ItemStack[] itemStacks;
+    private final boolean[] updateMask;
+
+    protected static void runExpensiveTableValidation(int[] translationTable) {
+        Set<Integer> indexSet = new HashSet<>();
+        for (int index : translationTable) {
+            indexSet.add(index);
+        }
+
+        Validate.isTrue(indexSet.size() == translationTable.length);
+    }
+
+    protected TranslationTableAdapter(int[] translationTable, Player player) {
+        this.translationTable = translationTable;
+
+        this.player = player;
+        this.playerInventory = player.getInventory();
+
+        this.length = playerInventory.getSize();
+        this.itemStacks = playerInventory.getContents();
+        this.updateMask = new boolean[length];
+
+        Validate.isTrue(length == InventoryConstants.PLAYER_INV_LENGTH);
+        Validate.isTrue(itemStacks.length == InventoryConstants.PLAYER_INV_LENGTH);
+    }
+
+    private TranslationTableAdapter(TranslationTableAdapter adapter) {
+        this.translationTable = adapter.translationTable;
+
+        this.player = adapter.player;
+        this.playerInventory = adapter.playerInventory;
+
+        this.length = adapter.length;
+        this.itemStacks = ItemUtil.clone(adapter.itemStacks);
+        this.updateMask = adapter.updateMask.clone();
+    }
+
+    @Override
+    public int size() {
+        return translationTable.length;
+    }
+
+    private int translateExternalIndexToInternal(int index) {
+        return translationTable[index];
+    }
+
+    @Override
+    public ItemStack getAt(int index) {
+        return itemStacks[translateExternalIndexToInternal(index)];
+    }
+
+    @Override
+    public void setAt(int index, ItemStack stack) {
+        itemStacks[translateExternalIndexToInternal(index)] = stack;
+        updateMask[translateExternalIndexToInternal(index)] = true;
+    }
+
+    @Override
+    public TranslationTableAdapter copy() {
+        return new TranslationTableAdapter(this);
+    }
+
+    @Override
+    public void applyChanges() {
+        for (int i = 0; i < length; ++i) {
+            if (updateMask[i]) {
+                playerInventory.setItem(i, itemStacks[i]);
+            }
+        }
+    }
+}
