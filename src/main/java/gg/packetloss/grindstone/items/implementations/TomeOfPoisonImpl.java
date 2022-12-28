285dc78a87876d1c6d7d19ac72fe234133f1402a
Wyatt Childers <wchilders@nearce.com>
2022-12-27 23:28:02 -0500

    Change out the Tome of Poison effect to the original design

--- src/main/java/gg/packetloss/grindstone/items/implementations/TomeOfPoisonImpl.java
+++ src/main/java/gg/packetloss/grindstone/items/implementations/TomeOfPoisonImpl.java
@@ -6,16 +6,21 @@
 
 package gg.packetloss.grindstone.items.implementations;
 
+import com.sk89q.commandbook.CommandBook;
 import gg.packetloss.grindstone.items.generic.AbstractItemFeatureImpl;
-import org.bukkit.Location;
+import gg.packetloss.grindstone.util.ChanceUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class TomeOfPoisonImpl extends AbstractItemFeatureImpl {
-    @EventHandler(ignoreCancelled = true)
-    public void onEntityDamageEvent(EntityDamageEvent event) {
-        if (event.getCause() != EntityDamageEvent.DamageCause.POISON) {
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onEntityDamageEvent(EntityPotionEffectEvent event) {
+        if (event.getAction() != EntityPotionEffectEvent.Action.ADDED &&
+            event.getAction() != EntityPotionEffectEvent.Action.CHANGED) {
             return;
         }
 
@@ -27,11 +32,22 @@ public class TomeOfPoisonImpl extends AbstractItemFeatureImpl {
             return;
         }
 
-        Location playerLoc = player.getLocation();
+        PotionEffect newEffect = event.getNewEffect();
+        if (newEffect == null || !newEffect.getType().equals(PotionEffectType.POISON)) {
+            return;
+        }
 
-        // If the player is "jumping" cancel the poison damage entirely.
-        if (playerLoc.getY() > playerLoc.getBlockY()) {
-            event.setCancelled(true);
+        if (ChanceUtil.getChance(5)) {
+            CommandBook.server().getScheduler().runTask(CommandBook.inst(), () -> {
+                if (player.getPotionEffect(PotionEffectType.REGENERATION) == null) {
+                    player.addPotionEffect(new PotionEffect(
+                        PotionEffectType.REGENERATION,
+                        Math.min(20 * 60 * 2, newEffect.getDuration()),
+                        Math.min(1, newEffect.getAmplifier())
+                    ));
+                }
+                player.removePotionEffect(PotionEffectType.POISON);
+            });
         }
     }
 }
